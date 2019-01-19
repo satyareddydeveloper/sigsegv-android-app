c8c119e9206e6532f33bd391a7e0dbe1f462748c
melianor <ibrahimsn98@gmail.com>
2019-01-19 23:43:03 +0300

    add login screen layout

--- app/src/main/java/com/sigsegv/doctor/ui/main/MainActivity.java
+++ app/src/main/java/com/sigsegv/doctor/ui/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.sigsegv.doctor.R;
+import com.sigsegv.doctor.ui.auth.AuthFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, AuthFragment.newInstance()).commit();
     }
 }
--- app/src/main/java/com/sigsegv/doctor/ui/start/StartActivity.java
+++ app/src/main/java/com/sigsegv/doctor/ui/start/StartActivity.java
@@ -1,9 +1,11 @@
 package com.sigsegv.doctor.ui.start;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.sigsegv.doctor.R;
+import com.sigsegv.doctor.ui.main.MainActivity;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
